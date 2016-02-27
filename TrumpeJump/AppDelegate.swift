56e7d75f48449e9495ebd4c958ec3b80c8316653
robinonsay <raomax@gmail.com>
2016-02-27 05:13:54 -0500

    changed to landscape

--- TrumpeJump/AppDelegate.swift
+++ TrumpeJump/AppDelegate.swift
@@ -16,10 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        PollData.getTrumpPollResults { (results) -> Void in
-            print("SUCCESS")
-        }
+        // Override point for customization after application launch
         return true
     }
 
